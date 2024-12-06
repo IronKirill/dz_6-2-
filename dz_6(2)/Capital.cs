825a2a1f464db08262bdca05d016bcd65dc51ad8
kiryl <kiryl@DESKTOP-3T3FAOL>
2024-12-06 23:30:02 +0200

    Add project files.

---
+++ dz_6(2)/Capital.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country1
+{
+    public class Capital
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+
+        public Capital(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
+
+namespace Country2
+{
+    public class Capital
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+
+        public Capital(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
+
+namespace Country3
+{
+    public class Capital
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+
+        public Capital(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
---
+++ dz_6(2)/Program.cs
@@ -0,0 +1,48 @@
+﻿using Country1;
+using Country2;
+using Country3;
+
+namespace dz_6_2_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var capital1 = new Country1.Capital("Kyiv", 3000000);
+            var capital2 = new Country2.Capital("Warsaw", 1865000);
+            var capital3 = new Country3.Capital("Rome", 2760000);
+
+            Console.WriteLine($"{capital1.Name}: " +
+                $"Population {capital1.Population}");
+
+            Console.WriteLine($"{capital2.Name}: " +
+                $"Population {capital2.Population}");
+
+            Console.WriteLine($"{capital3.Name}: " +
+                $"Population {capital3.Population}");
+
+            Console.WriteLine();
+
+            int maxPopulation = Math.Max(capital1.Population, Math.Max(capital2.Population, 
+                capital3.Population));
+
+            if (capital1.Population == maxPopulation)
+            {
+                Console.WriteLine($"The capital with the largest population is " +
+                    $"{capital1.Name} (Country1) with {capital1.Population} people.");
+            }
+            
+            else if (capital2.Population == maxPopulation)
+            {
+                Console.WriteLine($"The capital with the largest population is " +
+                    $"{capital2.Name} (Country2) with {capital2.Population} people.");
+            }
+            
+            else
+            {
+                Console.WriteLine($"The capital with the largest population is " +
+                    $"{capital3.Name} (Country3) with {capital3.Population} people.");
+            }
+        }
+    }
+}
